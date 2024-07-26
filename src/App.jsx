05df42404c5983786808db94ba660302a5ec2276
junjuny0227 <junjuny0227@gmail.com>
2024-07-26 13:19:52 +0900

    Modifying the Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import "./style/fonts.css";
 import "./style/global.css";
+import Making from "./pages/making";
+import Example from "./pages/example";
+import About from "./pages/about";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/making" element={<Making />} />
+        <Route path="/example" element={<Example />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
